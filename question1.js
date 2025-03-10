202152bfe00d7e164da114d46a21ddc20ffa9799
Raprap72 <wisrtalon@gmail.com>
2025-03-10 21:09:28 +0800

    question1.js

--- question1.js
+++ question1.js
@@ -2,6 +2,5 @@
 function filterEvenNumbers(numbers) {
     return numbers.filter(num => num % 2 === 0);
 }
-
 // Example usage
 console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6])); // Output: [2, 4, 6]
